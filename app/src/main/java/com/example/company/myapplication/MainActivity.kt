e11dc67b7644061843c1ce914b31dbaaf098313e
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-01 01:35:13 +0500

    Deleted useless imports

--- app/src/main/java/com/example/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.GridView
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
